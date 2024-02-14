1ba28b6ae3f67dda44cbd5cf6dc3027e553fc71c
onyxgeek <onyxpeek@gmail.com>
2024-02-14 10:41:10 +0200

    feat: finish the store

--- src/app/modules/about-us/about-us-routing.module.ts
+++ src/app/modules/about-us/about-us-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Home route
+  {
+    path: '',
+    component: AboutUsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
